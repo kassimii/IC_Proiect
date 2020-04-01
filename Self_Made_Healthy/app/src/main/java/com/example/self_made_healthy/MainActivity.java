93ff90af976a0c96436707544531f0ccfdb180f4
kassimii <patyboc@yahoo.com>
2020-04-01 23:12:48 +0300

    Added Meal Planning Activity Images

--- Self_Made_Healthy/app/src/main/java/com/example/self_made_healthy/MainActivity.java
+++ Self_Made_Healthy/app/src/main/java/com/example/self_made_healthy/MainActivity.java
@@ -6,16 +6,19 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button setMealsButton;
+    private Button accessProfileButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         onMealButtonClick();
+        onProfileButtonClick();
     }
 
     public void onMealButtonClick(){
@@ -33,5 +36,17 @@ public class MainActivity extends AppCompatActivity {
     public void openSetMealHoursActiviy(){
         Intent intent = new Intent(this, SetMealHours.class);
         startActivity(intent);
+    }
+
+    public void onProfileButtonClick(){
+        accessProfileButton = (Button) findViewById(R.id.profile_button);
+        accessProfileButton.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Toast.makeText(MainActivity.this,"Plm",Toast.LENGTH_SHORT).show();
+                    }
+                }
+        );
     }
 }
--- Self_Made_Healthy/app/src/main/java/com/example/self_made_healthy/SetMealHours.java
+++ Self_Made_Healthy/app/src/main/java/com/example/self_made_healthy/SetMealHours.java
@@ -11,39 +11,14 @@ import java.sql.Time;
 import android.widget.Toast;
 public class SetMealHours extends AppCompatActivity {
 
-    private EditText breakfast_hour, snack1_hour, lunch_hour, snack2_hour, dinner_hour;
-    private Button save_plan_button;
+//    private EditText breakfast_hour, snack1_hour, lunch_hour, snack2_hour, dinner_hour;
+//    private Button save_plan_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_set_meal_hours);
-       // onSavePlanButtonClick();
     }
 
-//    public void onSavePlanButtonClick(){
-//        breakfast_hour = (EditText)findViewById(R.id.breakfast_time);
-//        snack1_hour = (EditText) findViewById(R.id.snack1_time);
-//        lunch_hour = (EditText) findViewById(R.id.lunch_time);
-//        snack2_hour = (EditText) findViewById(R.id.snack2_time);
-//        dinner_hour = (EditText) findViewById(R.id.dinner_time);
 //
-//        save_plan_button = (Button)findViewById(R.id.save_plan_button);
-//
-//        final Time breakfast_time = (Time) breakfast_hour.getText();
-//
-//        save_plan_button.setOnClickListener(
-//                new View.OnClickListener() {
-//                    @Override
-//                    public void onClick(View v) {
-//                        Toast.makeText(SetMealHours.this, breakfast_time.toString(),Toast.LENGTH_SHORT).show();
-//                    }
-//                }
-//        );
-//
-//
-//
-//
-//    }
-
 }

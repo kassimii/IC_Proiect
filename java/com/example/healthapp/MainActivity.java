3f2a33c3dd37634861744b797c47cccebf43fd8e
deianapatrut <48941633+deianapatrut@users.noreply.github.com>
2020-04-02 17:30:19 +0300

    Add files via upload
    -Log In 
    -Sign Up
    -Forgot Password

---
+++ java/com/example/healthapp/MainActivity.java
@@ -0,0 +1,64 @@
+package com.example.healthapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+public class MainActivity extends Activity {
+    private static FragmentManager fragmentManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        fragmentManager = getSupportFragmentManager();
+
+        // If savedinstnacestate is null then replace login fragment
+        if (savedInstanceState == null) {
+            fragmentManager
+                    .beginTransaction()
+                    .replace(R.id.frameContainer, new Login(),
+                            Utils.Login_Fragment).commit();
+        }
+
+
+    }
+
+    private FragmentManager getSupportFragmentManager() {
+        return this.fragmentManager;
+    }
+
+    // Replace Login Fragment with animation
+    protected void replaceLoginFragment() {
+        fragmentManager
+                .beginTransaction()
+                .setCustomAnimations(R.anim.left_enter, R.anim.right_out)
+                .replace(R.id.frameContainer, new Login(),
+                        Utils.Login_Fragment).commit();
+    }
+
+    @Override
+    public void onBackPressed() {
+
+        // Find the tag of signup and forgot password fragment
+        Fragment SignUp_Fragment = fragmentManager
+                .findFragmentByTag(Utils.SignUp_Fragment);
+        Fragment ForgotPassword_Fragment = fragmentManager
+                .findFragmentByTag(Utils.ForgotPassword_Fragment);
+
+        // Check if both are null or not
+        // If both are not null then replace login fragment else do backpressed
+        // task
+
+        if (SignUp_Fragment != null)
+            replaceLoginFragment();
+        else if (ForgotPassword_Fragment != null)
+            replaceLoginFragment();
+        else
+            super.onBackPressed();
+    }
+}
